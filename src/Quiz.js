8885cd7fd32cf79887dae58d1552ae58c2cfecef
Muhammad Shahril <shahril@mtradeasia.com>
2021-08-23 14:55:33 +0800

    adding state

--- src/Quiz.js
+++ src/Quiz.js
@@ -15,7 +15,7 @@ class Quiz extends Component {
     }
     
     render() {
-        const isQuizEnd = ((this.quiz_position - 1) === quizData.quiz_questions.length);
+        const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length);
         return (
             <div>
                 {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>}
