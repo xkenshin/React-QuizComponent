fbcc594e5a618a2de485e590ca072ad28bca3718
Muhammad Shahril <shahril@mtradeasia.com>
2021-08-23 13:31:23 +0800

    pushed quiz #2

---
+++ src/Quiz.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+let quizData = require('./quiz_data.json')
+
+class Quiz extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            quiz_position: 1
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                <div className = "QuizQuestion">
+                    {quizData.quiz_questions.instruction_text}   
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Quiz
