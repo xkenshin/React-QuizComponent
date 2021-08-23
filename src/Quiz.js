cd498adc2032a7fc5da6a01ed928e6d327372e67
Muhammad Shahril <shahril@mtradeasia.com>
2021-08-23 13:33:52 +0800

    adding object 0

--- src/Quiz.js
+++ src/Quiz.js
@@ -16,7 +16,7 @@ class Quiz extends Component {
         return (
             <div>
                 <div className = "QuizQuestion">
-                    {quizData.quiz_questions.instruction_text}   
+                    {quizData.quiz_questions[0].instruction_text}   
                 </div>
             </div>
         );
