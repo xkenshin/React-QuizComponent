26b35a78856b036ca13736cfc303c47fa616d48d
Muhammad Shahril <shahril@mtradeasia.com>
2021-08-23 16:42:36 +0800

    adding reset

--- src/Quiz.js
+++ src/Quiz.js
@@ -14,6 +14,10 @@ class Quiz extends Component {
         }
     }
 
+    handleResetClick() {
+        this.setState({quiz_position: 1})
+    }
+
     showNextQuestion() {
         this.setState((state) => {
             return {quiz_position: state.quiz_position + 1}
@@ -24,7 +28,7 @@ class Quiz extends Component {
         const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length);
         return (
             <div>
-                {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
+                {isQuizEnd ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
             </div>
         )
     }
